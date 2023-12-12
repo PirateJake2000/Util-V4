c42d24d4d1a22f44c272d892dd834e677136cf1f
Cuh4 <roozzguy@gmail.com>
2023-12-12 13:05:43 +0000

    staying consistent with formatting

--- Src/Library/Events.lua
+++ Src/Library/Events.lua
@@ -1,56 +1,59 @@
 function Util.Events.Create(name)
     -- If Event already exists, return
-    if Util.Events[name] then
+    if Util.Events.List[name] then
         return
     end
 
     -- Create Event
-    Util.Events[name] = {}
+    Util.Events.List[name] = {}
 
     print("Created Event: " .. name)
 end
 
 function Util.Events.Invoke(name, ...)
     -- If Event doesn't exist, create it
-    if not Util.Events[name] then
+    if not Util.Events.List[name] then
         Util.Events.Create(name)
     end
 
     -- Invoke Events
-    for _, callback in pairs(Util.Events[name]) do
+    for _, callback in pairs(Util.Events.List[name]) do
         callback(...)
     end
 end
 
 function Util.Events.Hook(name, callback)
     -- If Event doesn't exist, create it
-    if not Util.Events[name] then
+    if not Util.Events.List[name] then
         Util.Events.Create(name)
     end
 
     -- Hook Event
-    table.insert(Util.Events[name], callback)
+    table.insert(Util.Events.List[name], callback)
 end
 
 function Util.Events.Unhook(name, callback)
     -- If Event doesn't exist, return
-    if not Util.Events[name] then
+    if not Util.Events.List[name] then
         return
     end
 
     -- Unhook Event
-    for i, v in pairs(Util.Events[name]) do
+    for i, v in pairs(Util.Events.List[name]) do
         if v == callback then
-            table.remove(Util.Events[name], i)
+            table.remove(Util.Events.List[name], i)
         end
     end
 end
 
 function Util.Events.Destroy(name)
     -- If Event doesn't exist, return
-    if not Util.Events[name] then
+    if not Util.Events.List[name] then
         return
     end
 
-    Util.Events[name] = nil
+    Util.Events.List[name] = nil
 end
+
+---@type table<string, table<integer, function>>
+Util.Events.List = {}
