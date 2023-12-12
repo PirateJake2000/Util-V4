c2db1cc0ade28571eee3753dda282fc0f0c1424b
Cuh4 <roozzguy@gmail.com>
2023-12-12 13:10:18 +0000

    intellisense stuffs

--- Src/Library/Commands.lua
+++ Src/Library/Commands.lua
@@ -1,3 +1,13 @@
+---@class UtilCommand
+---@field name string
+---@field aliases table<integer, string>
+---@field callback fun(full_message: string, user_peer_id: integer, is_admin: boolean, is_auth: boolean, args: table<integer, string>)
+
+---@param full_message string
+---@param user_peer_id integer
+---@param is_admin boolean
+---@param is_auth boolean
+---@param ... string
 function Util.Commands.Check(full_message, user_peer_id, is_admin, is_auth, ...)
     local args = { ... }
 
@@ -12,16 +22,24 @@ function Util.Commands.Check(full_message, user_peer_id, is_admin, is_auth, ...)
     end
 end
 
+---@param name string
+---@param aliases table<integer, string>
+---@param callback function
+---@return UtilCommand
 function Util.Commands.Create(name, aliases, callback)
     Util.Commands.List[name] = {
         name = name,
         aliases = aliases,
         callback = callback
     }
+
+    return Util.Commands.List[name]
 end
 
+---@param name string
 function Util.Commands.Destroy(name)
     Util.Commands.List[name] = nil
 end
 
+---@type table<string, UtilCommand>
 Util.Commands.List = {}
